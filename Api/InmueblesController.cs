e74b38b805c27fb70242c26f39ea35eda473ae19
Marcela Angella <43481279+marceFa@users.noreply.github.com>
2021-05-18 19:45:33 -0300

    api
    api

---
+++ Api/InmueblesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Inmobiliaria.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Inmobiliaria.Api
+{
+    [Route("api/[controller]")]
+    [Microsoft.AspNetCore.Authorization.Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class InmueblesController : Controller
+    {
+        private readonly DataContext contexto;
+
+        public InmueblesController(DataContext contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var usuario = User.Identity.Name;
+                return Ok(contexto.Inmuebles.Include(e => e.Propietarios).Where(e => e.Propietarios.Email == usuario));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var usuario = User.Identity.Name;
+                return Ok(contexto.Inmuebles.Include(e => e.Propietarios).Where(e => e.Propietarios.Email == usuario).Single(e => e.IdProp == id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> Post(Inmuebles entidad)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    entidad.IdProp = contexto.Propietarios.Single(e => e.Email == User.Identity.Name).IdProp;
+                    contexto.Inmuebles.Add(entidad);
+                    contexto.SaveChanges();
+                    return CreatedAtAction(nameof(Get), new { id = entidad.IdProp }, entidad);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Inmuebles entidad)
+        {
+            try
+            {
+                if (ModelState.IsValid && contexto.Inmuebles.AsNoTracking().Include(e => e.Propietarios).FirstOrDefault(e => e.IdProp == id && e.Propietarios.Email == User.Identity.Name) != null)
+                {
+                    entidad.IdProp = id;
+                    contexto.Inmuebles.Update(entidad);
+                    contexto.SaveChanges();
+                    return Ok(entidad);
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var entidad = contexto.Inmuebles.Include(e => e.Propietarios).FirstOrDefault(e => e.IdProp == id && e.Propietarios.Email == User.Identity.Name);
+                if (entidad != null)
+                {
+                    contexto.Inmuebles.Remove(entidad);
+                    contexto.SaveChanges();
+                    return Ok();
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("BajaLogica/{id}")]
+        public async Task<IActionResult> BajaLogica(int id)
+        {
+            try
+            {
+                var entidad = contexto.Inmuebles.Include(e => e.Propietarios).FirstOrDefault(e => e.IdProp == id && e.Propietarios.Email == User.Identity.Name);
+                if (entidad != null)
+                {
+                    entidad.CantAmbientes = -1;//cambiar por estado = 0
+                    contexto.Inmuebles.Update(entidad);
+                    contexto.SaveChanges();
+                    return Ok();
+                }
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
